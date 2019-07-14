e869a92566ebdfb71fc06d98c9a7cfc364ce5563
StarLikeRain <pengyaokang@qq.com>
2019-07-14 18:11:26 +0800

    准备可以写个随机循环

--- src/com/spider/starlikerain/Main.java
+++ src/com/spider/starlikerain/Main.java
@@ -1,10 +1,12 @@
 package com.spider.starlikerain;
 
+import com.spider.starlikerain.activity.RandomString;
 import com.spider.starlikerain.model.NewsWithRelated;
 import com.spider.starlikerain.model.UrlNews;
 import com.spider.starlikerain.model.UrlNewsReader;
 import com.spider.starlikerain.model.Viewable;
 import com.spider.starlikerain.view.ListViewer;
+
 
 import java.io.IOException;
 import java.util.*;
@@ -18,22 +20,33 @@ public class Main {
 
         Queue<NewsWithRelated> newsQueue = new LinkedList<NewsWithRelated>(); // 记录一个 NewsWithRelated hashMap 类型的列表 数据结构为 title, url
 
-        String startUrl = "https://readhub.me/topic/5bMmlAm75lD";
+        String startUrl = "https://readhub.cn/topic/7OiKBs8TcLA";
         NewsWithRelated startNews = UrlNewsReader.read(startUrl); // 获得了一个 hashMap<title, url>
 
         int count = 0; // 弄一个什么计数器
-        Set<String> visitedUrl = new HashSet<>(); // 弄一个 hashMap 记录访问过的 url
+        Set<String> visitedUrl = new HashSet<>(); // 弄一个 hashMap 记录需要访问过的 url
         visitedUrl.add(startUrl);
+
         newsQueue.add(startNews);
+
+
+        startUrl = "https://readhub.cn/topic/5bMmlAm75lD";
+        startNews = UrlNewsReader.read(startUrl); // 获得了一个 hashMap<title, url>
+
+        visitedUrl.add(startUrl);
+
+        newsQueue.add(startNews);
+
+
         ArrayList<Viewable> results = new ArrayList<>(); // Viewable 只是一个 interface 有 display
-        while(!newsQueue.isEmpty() && count <= maximumURL){
+        while (!newsQueue.isEmpty() && count <= maximumURL) {
             NewsWithRelated current = newsQueue.poll(); // 抓取队列第一个新闻
             results.add(current); // 往 ArrayList 里面加当前第一个
-            count ++;
-            for(Map.Entry<String, String> entry : current.getRelateds().entrySet()){
+            count++;
+            for (Map.Entry<String, String> entry : current.getRelateds().entrySet()) {
                 String url = entry.getValue(); // 这里 enrty 是单个 hashMap<String title, String url>;
                 NewsWithRelated next = UrlNewsReader.read(url);
-                if(!visitedUrl.contains(url)){ // 考虑不重复
+                if (!visitedUrl.contains(url)) { // 考虑不重复
                     newsQueue.add(next);
                     visitedUrl.add(url);
                 }
